4af1abdbf39644ec15d1534baf938a9960c0ebbe
Sarah Kreidler <sarah5280@gmail.com>
2012-01-10 21:46:23 +0000

    rev'd version in manifest

--- src/edu/cudenver/bios/filesvc/application/FileConstants.java
+++ src/edu/cudenver/bios/filesvc/application/FileConstants.java
@@ -28,6 +28,6 @@ package edu.cudenver.bios.filesvc.application;
  */
 public class FileConstants
 {
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "2.0.0";
     public static final String TAG_ERROR = "error";
 }
